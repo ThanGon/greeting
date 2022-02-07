92459495b2b794c3bf879c5eafcf1bf363b09064
ThanGon <nate.goncalves@gmail.com>
2022-02-07 15:50:57 -0300

    first

--- bin/greeting.dart
+++ bin/greeting.dart
@@ -1,20 +1,29 @@
 void main() {
   // Código principal
-  saudacoes('Nathan'/*, visitante: 'Leandro'*/);
+  saudacoes('Nathan', visitante: 'Leandro', corpo: funcao);
+}
+
+void funcao(int i) {
+  for(int j = 0; j < i; j++) {
+    print('Olá $j');
+  }
 }
 
 void saudacoes( // Função
   String nome, {
   bool mostrarHora = true,
-  String? visitante, // Variável Nullable
+  String? visitante = 'Não Informado', // Variável Nullable
+  required Function(int) corpo,
 }) {
+
+  corpo(15);
   print('Olá de ${nome.toUpperCase ()}');
-  // if(visitante != null) {
-  String v = visitante ?? 'Não Informado'; // Null Aware (caso seja valor null será substituido pelo valor padrão)
-  print('Bem-Vindo ${v.toUpperCase()}');
-    // }
+  if(visitante != null) {
+  // String v = visitante ?? 'Não Informado'; // Null Aware (caso seja valor null será substituido pelo valor padrão)
+  print('Bem-Vindo ${visitante.toUpperCase()}');
+    }
   if (mostrarHora) {
-    print('O horário é: ${hora()}');
+   print('O horário é: ${hora()}');
   }
 }
 
