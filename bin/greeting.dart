e01756ec4c87ef574a929e370b4f0f79224c2c14
ThanGon <nate.goncalves@gmail.com>
2022-02-08 11:12:42 -0300

    latest

--- bin/greeting.dart
+++ bin/greeting.dart
@@ -1,20 +1,40 @@
 void main() {
   // Código principal
-  saudacoes('Nathan'/*, visitante: 'Leandro'*/);
+  saudacoes('Nathan', visitante: 'Leandro', corpo: funcao);
+  String problema = gravidade(5) ?? 'Não informado';
+  print('A gravidade do seu problema é: $problema');
 }
 
-void saudacoes( // Função
+void funcao(int i) {
+  for (int j = 0; j < i; j++) {
+    print('Olá $j');
+  }
+}
+
+void saudacoes(
+  // Função
   String nome, {
   bool mostrarHora = true,
-  String? visitante, // Variável Nullable
+  String? visitante = 'Não Informado', // Variável Nullable
+  required Function(int) corpo,
+  // String problema = gravidade(5) ?? 'Não informado', /// BUG
 }) {
-  print('Olá de ${nome.toUpperCase ()}');
-  // if(visitante != null) {
-  String v = visitante ?? 'Não Informado'; // Null Aware (caso seja valor null será substituido pelo valor padrão)
-  print('Bem-Vindo ${v.toUpperCase()}');
-    // }
+  corpo(15);
+  print('Olá de ${nome.toUpperCase()}');
+  if (visitante != null) {
+    // String v = visitante ?? 'Não Informado'; // Null Aware (caso seja valor null será substituido pelo valor padrão)
+    print('Bem-Vindo ${visitante.toUpperCase()}');
+  }
   if (mostrarHora) {
     print('O horário é: ${hora()}');
+  }
+}
+
+String? gravidade(int x) {
+  if (x > 8) {
+    return 'Grave';
+  } else {
+    return 'Leve';
   }
 }
 
