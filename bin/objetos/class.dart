b45da3548dc64f4b1775e1a747d0f0d0d44f0198
ThanGon <nate.goncalves@gmail.com>
2022-02-15 15:50:59 -0300

    getters n setters

--- bin/objetos/class.dart
+++ bin/objetos/class.dart
@@ -1,7 +1,8 @@
 void main() {
   Pessoa pessoa1 = Pessoa(nome: 'Nathan', idade: 18, casado: false);
   print(pessoa1.aniversario());
-
+  pessoa1.dinheiro = 1000;
+  print(pessoa1.dinheiro);
   Pessoa pessoa2 = Pessoa(nome: 'Leandro', idade: 30, casado: true);
 
 }
@@ -16,7 +17,7 @@ class Pessoa {
   int idade;
   bool casado;
   
-  
+  double? _dinheiro;
 
   String aniversario() {
     print('Parabéns! $nome');
@@ -24,5 +25,15 @@ class Pessoa {
     idade++;
     
     return 'Você agora tem $idade anos';
+  }
+
+  set dinheiro(double? valor) {
+    print('Modificando dinheiro do $nome');
+    _dinheiro = valor;
+  }
+
+  double? get dinheiro {
+    print ('Lendo dinheiro do $nome');
+    return _dinheiro;
   }
 }
