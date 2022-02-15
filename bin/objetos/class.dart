36909a7894fc0b11d1d8f8df4910440baedf737b
ThanGon <nate.goncalves@gmail.com>
2022-02-15 10:06:43 -0300

    construtores

--- bin/objetos/class.dart
+++ bin/objetos/class.dart
@@ -1,28 +1,28 @@
 void main() {
-  Pessoa pessoa1 = Pessoa();
-  // pessoa1.nome = 'Gonçalves';
-  print(pessoa1.nome);
+  Pessoa pessoa1 = Pessoa(nome: 'Nathan', idade: 18, casado: false);
+  print(pessoa1.aniversario());
 
-  Pessoa pessoa2 = Pessoa();
-  pessoa2.nome = "Leandro";
-  pessoa2.casado = true;
-  print(pessoa2.nome);
-  print(pessoa2.casado);
+  Pessoa pessoa2 = Pessoa(nome: 'Leandro', idade: 30, casado: true);
 
-  /// Pessoa pessoa3 = Pessoa(); 
-  /// print(pessoa3.nome);
-  
 }
 
 class Pessoa {
-  String nome = 'Nathan';
-  int? idade;
-  bool? casado;
 
-  void aniversario() {
+  Pessoa({required this.nome, required this.idade, required this.casado}) {
+    print('O usuário é reconhecido como $nome tem $idade anos e se encontra $casado');
+  }
+
+  String nome = 'Nathan';
+  int idade;
+  bool casado;
+  
+  
+
+  String aniversario() {
     print('Parabéns! $nome');
-    if(idade !=null) {
-      idade = idade!+ 1;
-    }
+    
+    idade++;
+    
+    return 'Você agora tem $idade anos';
   }
 }
