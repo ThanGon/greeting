b244f40c6b4403d853cfffbca2b3d41b17e0eee7
ThanGon <nate.goncalves@gmail.com>
2022-02-21 15:27:13 -0300

    close

--- bin/objetos/heranca.dart
+++ bin/objetos/heranca.dart
@@ -1,43 +1,44 @@
 void main() {
-  Cachorro cachorro1 = Cachorro();
-  cachorro1.idade = 10;
-  cachorro1.nome = 'Sparky';
+  Cachorro cachorro1 = Cachorro('Sparky', 10);
   print(cachorro1);
   cachorro1.latir();
 
-  Gato baguera = Gato();
-  baguera.nome = 'Baguera';
-  // print('Seu gato se chama ${baguera.nome}');
-  baguera.arranhar();
+  Gato gatinho = Gato('Pst Pst Pst', 16);
+  print(gatinho);
+  gatinho.arranhar();
 
-  List<Animal> animais = [];
+  /*List<Animal> animais = [];
   animais.add(cachorro1);
-  animais.add(baguera);
+  animais.add(gatinho);*/ 
+
+  // print(cachorro1.hashCode); /  sempre herda as propriedades object por isso o método hashcode
 }
 
-class Animal {
+abstract class Animal {   // Classe abstrata não pode ser usada como objeto
   // Superclass onde as outras class vão herdar
-  // Animal({required this.nome, required this.idade});
-  String nome = 'Não identificado';
-  int? idade;
+  Animal(this.nome, this.idade);
+  String nome;
+  int idade;
 
   /*String identificar() {        /// Comentado para remover redundância
     return 'Nome do seu animal: $nome. Idade do seu animal: $idade';
   }*/
 
-  @override
+  @override // Reescrita de método
   String toString() {
     return 'Nome: $nome Idade: $idade';
   }
 }
 
 class Cachorro extends Animal {
+  Cachorro(String nome, int idade) : super(nome, idade); // Passar da classe super para a classe que herda
   void latir() {
     print('Au au');
   }
 }
 
 class Gato extends Animal {
+  Gato(String nome, int idade) : super(nome, idade);
   void arranhar() {
     print('Seu gato te arranhou');
   }
